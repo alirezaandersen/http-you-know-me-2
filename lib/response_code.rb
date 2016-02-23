52b9d2f5a5332878ce3cc4b27cde3f2b3afe02a3
Alireza Andersen <alireza.andersen@gmail.com>
2016-02-22 21:53:24 -0700

    update

---
+++ lib/response_code.rb
@@ -0,0 +1,8 @@
+module ResponseCodes
+STATUS_OK       = '200 OK'
+STATUS_REDIRECT = '301 Moved Permanently'
+STATUS_MOVED    = '302 Found'
+STATUS_UNAUTH   = '401 Unauthorized'
+STATUS_FORB     = '403 Forbidden'
+STATUS_NOTFOUND = '404 Not Found'
+STATUS_ERROR    = '500 Internal Server Error'
--- lib/responsecodes.rb
+++
@@ -1,9 +0,0 @@
-module ResponseCodes
-  STATUS_OK = '200 OK'
-  STATUS_REDIRECT = '302 Found'
-  STATUS_MOVED = '301 Moved Permanently'
-  STATUS_UNAUTH = '401 Unauthorized'
-  STATUS_FORB = '403 Forbidden'
-  STATUS_NOTFOUND = '404 Not Found'
-  STATUS_ERROR = '500 Internal Server Error'
-end
